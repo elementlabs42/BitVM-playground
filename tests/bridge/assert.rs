5bd861345097ca70dd8ce8391be968576a9361a5
aristotleee <148852099+aristotleee@users.noreply.github.com>
2024-06-20 17:59:06 +0800

    add test

--- tests/bridge/assert.rs
+++ tests/bridge/assert.rs
@@ -2,7 +2,7 @@ use super::setup::setup_test;
 use bitcoin::{consensus::encode::serialize_hex, Amount, Network, OutPoint};
 use bitvm::bridge::{
     components::{
-        assert::AssertTransaction, bridge::BridgeTransaction, connector_b::ConnectorB,
+        assert::AssertTransaction, serialization::Serialization, bridge::BridgeTransaction, connector_b::ConnectorB,
         helper::Input,
     },
     graph::ONE_HUNDRED,
@@ -49,4 +49,42 @@ async fn test_assert_tx() {
     println!("Broadcast result: {:?}\n", result);
     println!("Transaction hex: \n{}", serialize_hex(&tx));
     assert!(result.is_ok());
+}
+
+#[tokio::test]
+async fn test_assert_tx_serialization() {
+    let (client, context) = setup_test();
+
+    let connector_b = ConnectorB::new(
+        Network::Testnet,
+        &context.n_of_n_taproot_public_key.unwrap(),
+    );
+
+    let input_value = Amount::from_sat(ONE_HUNDRED * 2 / 100);
+    let funding_utxo = client
+        .get_initial_utxo(connector_b.generate_taproot_address(), input_value)
+        .await
+        .unwrap_or_else(|| {
+            panic!(
+                "Fund {:?} with {} sats at https://faucet.mutinynet.com/",
+                connector_b.generate_taproot_address(),
+                input_value.to_sat()
+            );
+        });
+    let funding_outpoint = OutPoint {
+        txid: funding_utxo.txid,
+        vout: funding_utxo.vout,
+    };
+
+    let mut assert_tx = AssertTransaction::new(
+        &context,
+        Input {
+            outpoint: funding_outpoint,
+            amount: input_value,
+        },
+    );
+
+    assert_tx.pre_sign(&context);
+
+    assert_eq!(assert_tx.serialize(), ">>>>>>>>>>>>>>>>>>>> object serialized");
 }
