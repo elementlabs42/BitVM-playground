339c3f33f27852aabfbe2318f8b887f17f06e40b
franklynot <148856869+franklynot@users.noreply.github.com>
2024-07-31 15:30:08 +0200

    Add n-of-n pubkey generation

--- tests/bridge/client/musig2_keys.rs
+++ tests/bridge/client/musig2_keys.rs
@@ -1,15 +1,27 @@
-use musig2::secp::Scalar;
+use musig2::{
+    secp::{Point, Scalar},
+    KeyAggContext,
+};
 
 #[tokio::test]
 async fn generate_signer_keys() {
-    let secret = Scalar::random(&mut rand::rngs::OsRng);
-    let secret_string: String = secret
-        .serialize()
-        .iter()
-        .map(|byte| format!("{:02x}", byte))
-        .collect();
-    let public_key = secret.base_point_mul();
+    let mut public_keys: Vec<Point> = Vec::new();
+    for i in 0..2 {
+        let secret = Scalar::random(&mut rand::rngs::OsRng);
+        let secret_string: String = secret
+            .serialize()
+            .iter()
+            .map(|byte| format!("{:02x}", byte))
+            .collect();
+        let public_key = secret.base_point_mul();
+        public_keys.push(public_key);
 
-    println!("Private key:\t{}", secret_string);
-    println!("Public key:\t{}", public_key);
+        println!("Signer{i} private key:\t{secret_string}");
+        println!("Signer{i} public key:\t{public_key}");
+    }
+
+    let key_agg_ctx = KeyAggContext::new(public_keys).unwrap();
+    let aggregated_key: Point = key_agg_ctx.aggregated_pubkey();
+
+    println!("N-of-n public Key:\t{aggregated_key}");
 }
