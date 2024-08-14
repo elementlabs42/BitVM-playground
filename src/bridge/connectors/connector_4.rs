a306c6cb05ff2cf68f5a72377cc8685102a7d816
ivebeenherebefore <148863352+ivebeenherebefore@users.noreply.github.com>
2024-08-14 08:35:57 -0500

    Start v3 graph

---
+++ src/bridge/connectors/connector_4.rs
@@ -0,0 +1,50 @@
+use bitcoin::{Address, Network, PublicKey, ScriptBuf, Sequence, TxIn};
+use serde::{Deserialize, Serialize};
+
+use super::{
+    super::{
+        super::bridge::constants::NUM_BLOCKS_PER_2_WEEKS, scripts::*, transactions::base::Input,
+    },
+    connector::*,
+};
+
+#[derive(Serialize, Deserialize, Eq, PartialEq, Clone)]
+pub struct Connector4 {
+    pub network: Network,
+    pub operator_public_key: PublicKey,
+    pub num_blocks_timelock: u32,
+}
+
+impl Connector4 {
+    pub fn new(network: Network, operator_public_key: &PublicKey) -> Self {
+        Connector4 {
+            network,
+            operator_public_key: operator_public_key.clone(),
+            num_blocks_timelock: if network == Network::Bitcoin {
+                NUM_BLOCKS_PER_2_WEEKS
+            } else {
+                1
+            },
+        }
+    }
+}
+
+impl P2wshConnector for Connector4 {
+    fn generate_script(&self) -> ScriptBuf {
+        generate_timelock_script(&self.operator_public_key, self.num_blocks_timelock)
+    }
+
+    fn generate_address(&self) -> Address {
+        generate_timelock_script_address(
+            self.network,
+            &self.operator_public_key,
+            self.num_blocks_timelock,
+        )
+    }
+
+    fn generate_tx_in(&self, input: &Input) -> TxIn {
+        let mut tx_in = generate_default_tx_in(input);
+        tx_in.sequence = Sequence(u32::try_from(NUM_BLOCKS_PER_2_WEEKS).ok().unwrap() & 0xFFFFFFFF);
+        tx_in
+    }
+}
