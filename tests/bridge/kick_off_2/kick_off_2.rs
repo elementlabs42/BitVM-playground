919cf86f9964a580f93288fb43a53fee46abf19e
StarDumpling <148853647+StarDumpling@users.noreply.github.com>
2024-10-22 16:13:45 +0200

    Start with `Header` struct from bitcoin lib

--- tests/bridge/kick_off_2/kick_off_2.rs
+++ tests/bridge/kick_off_2/kick_off_2.rs
@@ -1,4 +1,9 @@
-use bitcoin::Amount;
+use std::str::FromStr;
+
+use bitcoin::{
+    block::{Header, Version},
+    Amount, BlockHash, CompactTarget, TxMerkleNode,
+};
 
 use bitvm::bridge::{
     connectors::base::TaprootConnector,
@@ -37,6 +42,24 @@ async fn test_kick_off_2_tx() {
         time: 45678,
         weight: 9012345,
     };
+
+    // TODO: check whether the block hash matches the actual block No. 866191 hash
+    let header = Header {
+        version: Version::from_consensus(0x200d2000),
+        prev_blockhash: BlockHash::from_str(
+            "000000000000000000027c9f5b07f21e39ba31aa4d900d519478bdac32f4a15d",
+        )
+        .unwrap(),
+        merkle_root: TxMerkleNode::from_str(
+            "0064b0d54f20412756ba7ce07b0594f3548b06f2dad5cfeaac2aca508634ed19",
+        )
+        .unwrap(),
+        time: 1729244761,
+        bits: CompactTarget::from_hex("0x17030ecd").unwrap(),
+        nonce: 0x400e345c,
+    };
+    let block_hash = header.block_hash();
+
     kick_off_2_tx.sign_input_0(
         &config.operator_context,
         &config.connector_1,
